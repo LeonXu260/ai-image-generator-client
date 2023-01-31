d4afd4236e03efbd8e58893b1af452c9e427147d
Leon Xu <dcwinfield362@gmail.com>
2023-01-30 18:01:18 -0800

    doc: change the backend url to the latest one on render.com

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -25,7 +25,7 @@ const CreatePost = () => {
 
       try {
         const response = await fetch(
-          "https://dalle-image-generator-server.onrender.com/api/v1/posts",
+          "https://a-s5dw.onrender.com/api/v1/posts",
           {
             method: "POST",
             headers: {
@@ -61,7 +61,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "https://dalle-image-generator-server.onrender.com/api/v1/dalle",
+          "https://a-s5dw.onrender.com/api/v1/dalle",
           {
             method: "POST",
             headers: {
